1f3ebb519400b66933059a9a71a5966511054720
Prince Kumar <prince736804545@gmail.com>
2024-06-04 07:43:50 +0530

    update __init__.py

--- myhelper/__init__.py
+++ myhelper/__init__.py
@@ -1,3 +1,17 @@
+"""Code Snippets that I regularly use"""
+
+__all__ = [
+    "Ytube",
+    "pdftk,
+    "generate_random_string",
+    "hbs",
+    "make_filename_safe",
+    "do_it_async",
+    "main"
+]
+__author__ = "Prince Kumar"
+__version__ = "0.1.5-alpha"
+
 from .pdftk import *
 from .utils import *
 from .yt_dlp_helper import *
